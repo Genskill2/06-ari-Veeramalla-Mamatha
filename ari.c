3cd17eb9466ba858b9f7df621d581cad2ddfd7b3
Veeramalla-Mamatha <84660873+Veeramalla-Mamatha@users.noreply.github.com>
2021-06-30 22:15:23 +0530

    Update ari.c

--- ari.c
+++ ari.c
@@ -1,124 +1,122 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<ctype.h>
-#include<math.h>
-#include<assert.h>
-#include<string.h>
-#include<cs50.h>
+#include <string.h>
+#include <cs50.h>
+#include <ctype.h>
 
-string ari(string str)
+int letter_count(string s)
 {
-    int i;
-    int c = 0;
-    int w = 0;
-    int s = 0;
+	int l= strlen(s);
+	int num_letters=0;
+	for(int i=0; i<l; i++)
+	{
+	if(isalnum(s[i]))
+	{
+		num_letters++;
+	}
+	}
+	return num_letters;
+}
 
-    int index = 0;
+int word_count(string s)
+{
+	int l=strlen(s);
+	int num_words=0;
+	for(int i=0; i<l; i++)
+	{
+	if(s[i]==' ')
+	{
+		num_words++;
+	}
+	}
+	return num_words+1;
+}
 
-    for(i=0;i<strlen(str);i++)
-    {
-        if(isalnum(str[i]))
-        {
-            c++;
-        }
-        else if(isspace(str[i]))
-        {
-            w++;
-        }
-        else if(str[i] == '.' || str[i] == '!' || str[i] == '?')
-        {
-            s++;
-        }
-    }
+int sentences_count(string s)
+{
+	int l=strlen(s);
+	int num_sentences=0;
+	int temp=0;
+	for(int i=0; i<l; i++)
+	{
+	if(s[i]=='.'||s[i]=='?'||s[i]=='!')
+	{
+		num_sentences++;
+	}
+	if(s[i]=='(')
+	{
+	for(int j=i; s[j]!=')'; j++)
+	{
+		if(s[j]=='.'||s[j]=='?'||s[j]=='!')
+	{
+		temp++;
+	}
+	}
+	}
+	}
+	return num_sentences-temp;
+}
 
-    index = (ceil)((4.71 * c/w) + (0.5 * w/s) - 21.43);
-
-    switch(index)
-    {
-        case 1 : 
-        {
-            return "Kindergarten";
-            break;
-        }
-
-        case 2 : 
-        {
-            return "First/Second Grade";
-            break;
-        }
-
-        case 3 : 
-        {
-            return "Third Grade";
-            break;
-        }
-
-        case 4 : 
-        {
-            return "Fourth Grade";
-            break;
-        }
-
-        case 5 : 
-        {
-            return "Fifth Grade";
-            break;
-        }
-
-        case 6 : 
-        {
-            return "Sixth Grade";
-            break;
-        }
-
-        case 7 : 
-        {
-            return "Seventh Grade";
-            break;
-        }
-
-        case 8 : 
-        {
-            return "Eighth Grade";
-            break;
-        }
-
-        case 9 : 
-        {
-            return "Ninth Grade";
-            break;
-        }
-
-        case 10 : 
-        {
-            return "Tenth Grade";
-            break;
-        }
-
-        case 11 : 
-        {
-            return "Eleventh Grade";
-            break;
-        }
-
-        case 12 : 
-        {
-            return "Twelfth grade";
-            break;
-        }
-
-        case 13 : 
-        {
-            return "College student";
-            break;
-        }
-
-        case 14 : 
-        {
-            return "Professor";
-            break;
-        }
-    }
-
-    return NULL;
+string ari(string s)
+{
+	int letters=letter_count(s);
+	int words=word_count(s);
+	int sentences=sentences_count(s);
+	int RI = (int)((4.71*((float)letters/words))+(0.5*((float)words/sentences))-21.43)+1;
+	string grade;
+	if(RI==1)
+	{
+		grade = "Kindergarten";
+	}
+	else if(RI==2)
+	{
+		grade = "First/Second Grade";
+	}
+	else if(RI==3)
+	{
+		grade = "Third Grade";
+	}
+	else if(RI==4)
+	{
+		grade = "Fourth Grade";
+	}
+	else if(RI==5)
+	{
+		grade = "Fifth Grade";
+	}
+	else if(RI==6)
+	{
+		grade = "Sixth Grade";
+	}
+	else if(RI==7)
+	{
+		grade = "Seventh Grade";
+	}
+	else if(RI==8)
+	{
+		grade = "Eighth Grade";
+	}
+	else if(RI==9)
+	{
+		grade = "Ninth Grade";
+	}
+	else if(RI==10)
+	{
+		grade = "Tenth Grade";
+	}
+	else if(RI==11)
+	{
+		grade = "Eleventh Grade";
+	}
+	else if(RI==12)
+	{
+		grade = "Twelfth Grade";
+	}
+	else if(RI==13)
+	{
+		grade = "College student";
+	}
+	else
+	{
+		grade = "Professor";
+	}
+	return grade;
 }
